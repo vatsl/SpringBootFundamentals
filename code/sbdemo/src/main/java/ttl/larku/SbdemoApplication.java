8d9a5fe9a24bfb3f378fea1d6dd3a106c66f5750
whynot <anil@panix.com>
2020-10-21 14:04:59 -0400

    Controllers 2

---
+++ code/sbdemo/src/main/java/ttl/larku/SbdemoApplication.java
@@ -0,0 +1,42 @@
+package ttl.larku;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Component;
+import ttl.larku.domain.Student;
+import ttl.larku.service.StudentService;
+
+import java.util.List;
+
+//@SpringBootApplication(scanBasePackages = {"ttl.larku.jconfig", "ttl.larku.sbdemo"})
+@SpringBootApplication //(scanBasePackages = {"ttl.larku"})
+public class SbdemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SbdemoApplication.class, args);
+    }
+
+}
+
+@Component
+class MyRunner implements CommandLineRunner
+{
+
+	public final StudentService studentService;
+
+    public MyRunner(StudentService studentService) {
+        this.studentService = studentService;
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        System.out.println("Hello from MyRunner");
+
+        List<Student> students = studentService.getAllStudents();
+        System.out.println("students:");
+        students.forEach(System.out::println);
+
+    }
+}
